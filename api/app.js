7a325d2df34a97e32505afe0aff187699cbf9085
marcos <marcosalvarezcalabria@gmail.com>
2024-08-08 17:15:06 +0100

    complet CRUD + LOGIN with JWT

--- api/app.js
+++ api/app.js
@@ -1,14 +1,24 @@
-require ('dotenv').config();
+require("dotenv").config();
 
-const express = require ('express');
-const logger = require('morgan');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("./midlewares/cors.middleware")
 
-require('./configs/db.configs')
+require("./configs/db.configs")
 
 const app = express();
+//Middlewares
 
-app.use(logger('dev'));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cors);
+
+//Routes
+
+app.use("/api", require ("./configs/routes.config"));
+
+
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.info(`Application runnig in port ${port}`))
+app.listen(port, () => console.info(`Application running in port ${port}`))
